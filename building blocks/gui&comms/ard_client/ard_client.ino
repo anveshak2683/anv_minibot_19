6447e62e27dd669add0be4e9e0aae3ac75b1ee91
vemuri0411 <pavanvemuri411@gmail.com>
2019-03-17 13:07:59 +0530

    ard_client

--- building blocks/gui&comms/ard_client/ard_client.ino
+++ building blocks/gui&comms/ard_client/ard_client.ino
@@ -1,19 +1,25 @@
 #include<SPI.h>
 #include <Ethernet.h>
 
+//IP of shield
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 IPAddress ip(10,42,0,5);
+
+//Server
 IPAddress server(10,42,0,3);
+int port  = 3000;
+
 EthernetClient client;
-char c;
-int d;
+
+char c[2];
+int right,left;
 
 void setup() 
 {
  Ethernet.begin(mac, ip);
  pinMode(13, OUTPUT);
  pinMode(4, OUTPUT);
- while(!client.connect(server, 10005))
+ while(!client.connect(server, port))
  {}
  /*TCCR0A = _BV(COM0A0) | _BV(COM0B1) | _BV(WGM00)|_BV(WGM01);
  TCCR2B = _BV(CS02)| _BV(CS01)| _BV(CS00);
@@ -25,15 +31,14 @@ void loop()
 {
   if(!client.connected())
   {
-    while(!client.connect(server,10005))
+    while(!client.connect(server,port))
     {}    
   }
   else if (client.available()) 
   {
-    c=client.read();
-    d=int(c);
-    /*c = client.readStringUntil('\n');
-    d = c.toInt();
-    OCR2B= map(d, 0, 1023, 12, 255);*/
+    c = client.readStringUntil('\n');
+    right = int(c[0]);
+    left = int(c[1]);
+    //OCR2B= map(d, 0, 1023, 12, 255);
   }
 }
